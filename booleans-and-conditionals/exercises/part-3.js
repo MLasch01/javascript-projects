caed26a96fb302d571bc0da485719ffddba05a38
mlasch01 <mark.lasch@yahoo.com>
2024-06-17 19:53:36 -0500

    testing

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -1,6 +1,66 @@
-let engineIndicatorLight = 'red blinking';
-let fuelLevel = 21000;
-let engineTemperature = 1200;
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+ } else if (engineIndicatorLight === "green blinking") {
+    console.log("engines are preparing to start");
+ } else {
+    console.log("engines are off");
+ }
+ if (crewStatus) {
+    console.log("Crew Ready");
+ } else {
+    console.log("Crew Not Ready");
+ }
+ if (computerStatusCode === 200) {
+    console.log("Please stand by. Computer is rebooting");
+ } else if (computerStusCode === 400) {
+    console.log("Success! Computer online.");
+ } else {
+    console.log("Alert: Computer offline!");
+ }
+ if (shuttleSpeed > 17500) {
+    console.log("ALERT: Escape velocity reached!");
+ } else if (shuttleSpeed < 8000) {
+    console.log("ALERT: Cannot maintain orbit!");
+ } else {
+    console.log("Stable speed");
+ }
+ if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
+ let fuelLevel = 21000;
+ let engineTemperature = 1200
+ let commandOverride = true
+ if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking") {
+    console.log("ENGINE FAILURE IMMINENT!");
+ } else if (fuelLevel <= 5000 || engineTemperature > 2500) {
+    console.log("Check fuel level. Engines running hot.");
+ } else if (fuelLevel > 20000 && engineTemperature <= 2500) {
+    console.log("Full tank. Engines good.");
+ } else if (fuelLevel > 10000 && engineTemperature <= 2500) {
+    console.log("Fuel level above 50%. Engines good.");
+ } else if (fuelLevel > 5000 && engineTemperature <= 2500) {
+    console.log("Fuel Level above 25%. Engines Good.");
+ } else {
+    console.log("Fuel and engine status pending...");
+ }
+ if (fuelLevel > 2000 && engineIndicatorLight !== "red blinking" || commandOverride === true) {
+    console.log("Cleared to Launch!");
+ } else {
+    console.log("Launch scrubbed!");
+ }
 
 /* 5) Implement the following checks using if/else if/else statements:
 
---
+++ data-and-variables/exercises/console.js
@@ -0,0 +1,9 @@
+console.log(1 + 5 % 3);
+console.log(2 ** 2 ** 3 * 3);
+console.log(16 - 2 * 5 / 3 +1);
+const input = require('readline-sync');
+
+let info = input.question("Please enter your age: ");
+//The user enters 25.
+
+console.log(typeof info);
