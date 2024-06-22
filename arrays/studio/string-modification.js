f3f1b1d0544a41f09ced62be991412d4fa2d4b3e
mlasch01 <mark.lasch@yahoo.com>
2024-06-22 16:33:44 -0500

    exercise complete

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,7 +3,9 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let newStr;
+newStr = str.splice(3) + str.push(Lau);
+console.log(newStr);
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,46 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
-
+let fuel = 0
+let astronauts = 0
+let altitude = 0
 
 
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+const input = require('readline sync');
 
+fuel = input.question ('Please enter a Fuel Level:');
+while (fuel <= 5000 || fuel > 30000 || isNaN(fuel)) {
+  fuel = input.question ('Invalid input.  Please enter a valid Fuel Level:');
+}
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  
+astronauts = input.question ('Please enter number of astronauts:');
+while (astronauts <= 1 || astronauts > 7 || isNaN(astronauts)) {
+  fuel = input.question ('Invalid input.  Please enter a valid number of astronauts:');
+}
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
 
-
+while (fuel-100*astronauts >=0) {
+  altitude += 50;
+  fuel -= 100*astronauts;
+}
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+let output = `The shuttle gained an altitude of ${altitude} km.`;
+if (altitude >= 2000) {
+  output += "Orbit achieved!";
+}
+else {
+  console.log("Failed to reach orbit.");
+} 
