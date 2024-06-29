a5453fa2acc925f9191d7b1cfabeffc525f4dfc1
mlasch01 <mark.lasch@yahoo.com>
2024-06-29 10:16:21 -0500

    practice

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -1,25 +1,63 @@
-let str = 'In space, no one can hear you code.';
-let arr = ['B', 'n', 'n', 5];
-let purpose = [];
-let newPurpose = '';
+let str = 'In space, no one can hear you code.'
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-purpose = str.split(',');
-console.log(purpose);
-purpose = str.split(' ');
-console.log(purpose);
-purpose = str.split('');
-console.log(purpose);
-//2) Use the join method on the array to identify the purpose of the parameter inside the ().
-newPurpose = arr.join("+");
-console.log(newPurpose);
-newPurpose = arr.join("");
-console.log(newPurpose);
-newPurpose = arr.join("_");
-console.log(newPurpose);
-//3) Do split or join change the original string/array?
+
+console.log(str.split());
+console.log(str.split('e'));
+console.log(str.split(' '));
+console.log(str.split(''));
 console.log(str);
+
+//2) Use the join method on the array to identify the purpose of the parameter inside the ().
+
+let arr = ['B', 'n', 'n', 5];
+
+console.log(arr.join());
+console.log(arr.join('a'));
+console.log(arr.join(' '));
+console.log(arr.join(''));
 console.log(arr);
+
+//3) Do split or join change the original string/array?
+
+
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
+
 let cargoHold = "water,space suits,food,plasma sword,batteries";
 console.log(cargoHold.split(',').sort().join(','));
+console.log(cargoHold);
+
+
+
+
+
+
+
+
+
+
+// let str = 'In space, no one can hear you code.';
+// let arr = ['B', 'n', 'n', 5];
+// let purpose = [];
+// let newPurpose = '';
+
+// //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+// purpose = str.split(',');
+// console.log(purpose);
+// purpose = str.split(' ');
+// console.log(purpose);
+// purpose = str.split('');
+// console.log(purpose);
+// //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+// newPurpose = arr.join("+");
+// console.log(newPurpose);
+// newPurpose = arr.join("");
+// console.log(newPurpose);
+// newPurpose = arr.join("_");
+// console.log(newPurpose);
+// //3) Do split or join change the original string/array?
+// console.log(str);
+// console.log(arr);
+// //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
+// let cargoHold = "water,space suits,food,plasma sword,batteries";
+// console.log(cargoHold.split(',').sort().join(','));
