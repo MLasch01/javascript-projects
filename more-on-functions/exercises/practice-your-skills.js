b50caf81059131fe5e904d04d2f9f000e4320d17
mlasch01 <mark.lasch@yahoo.com>
2024-06-30 15:37:36 -0500

    completed

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,6 +6,17 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+let process = function (n) {
+    if (typeof n === 'number') {
+        return n*3;
+    } else if (typeof n === 'string') {
+        return "ARRR!";
+    } else {
+            return (n)
+        }
+    };
+console.log(process);
+
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +24,17 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let test = arr.map(process);
+process = function (arr) {
+    if (typeof test === Number) {
+        return arr*3;
+    } else if (typeof test === "string") {
+        return "ARRR!";
+    } else {
+            return (test)
+        }
+    };
+
+console.log(arr);
+console.log(test);
