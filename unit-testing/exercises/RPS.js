6d6976d64057f99b40b3ab4c2fb88826a1b84e4c
mlasch01 <mark.lasch@yahoo.com>
2024-07-14 16:12:50 -0500

    completed

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -12,9 +12,17 @@ function whoWon(player1,player2){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'scissors' && player2 === 'rock '){
+   if (player1 === 'scissors' && player2 === 'rock'){
      return 'Player 2 wins!';
    }
- 
-   return 'Player 1 wins!';
+
+   if (player1 === 'fork' && player2 === 'soup'){
+      return 'Player 2 wins!';
+      
+   }
+   else{
+    return 'Player 1 wins!';
  }
+}
+
+ module.exports = whoWon;
